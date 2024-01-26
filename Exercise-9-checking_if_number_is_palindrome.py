f250db762d16f56ab52cec00b1ccc2b26ed065d0
unknown <miggy.payumo26@gmail.com>
2024-01-26 23:03:13 +0800

    Code finalized and labeled

--- Exercise-9-checking_if_number_is_palindrome.py
+++ Exercise-9-checking_if_number_is_palindrome.py
@@ -5,6 +5,8 @@
 # 4. if yes return "TRUE"
 
 while True:
+
+# Defining function for checking if number is palindrome.
     def given_is_palindrome(given_number):
         given_number = str(given_number)
 
@@ -14,7 +16,8 @@ while True:
             return True
         else:
             return False
-    
+
+# Asking for input from user.
     given_number = input("Give us a number: ")
     if given_number.isdigit():
         given_number = int(given_number)
@@ -23,6 +26,7 @@ while True:
         print("Invalid, input must be number.")
         break
 
+# Checking to see if the number inputted by user is palindrome.
     if given_is_palindrome(given_number):
         print(given_number, "<- Palindrome!")
     else:
